16f432b2a448c56cf2cd3e439f512b6eef1968a4
Achess <jealtzo26@gmail.com>
2022-07-31 12:37:53 -0600

    Removing basename in browser router

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
   const lang = useState(languages[0].lang);
   return (
     <LanguageContext.Provider value={lang}>
-      <BrowserRouter basename="Valorant-Info">
+      <BrowserRouter>
         <Header />
         <Routes>
           <Route path="/">
